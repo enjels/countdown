7dd7e599ac6919ea728116f5ea7b69ebd8db95ec
Virgílio César <enjels@outlook.com>
2024-03-12 13:23:05 -0300

    adicionado  componente Counter no App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
-import Title from "./components/Title"
+import Title from "./components/Title";
+import Counter from "./components/Counter";
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <Title title= "Contagem regreciva para 2023"/>
+        <Title title="Contagem regreciva para 2023" />
         <div className="countdown-container">
-          <p>2 dias</p>
+          <Counter title="Dias" number={4} />
+          <Counter title="Horas" number={4} />
+          <Counter title="Minutos" number={4} />
+          <Counter title="Segundos" number={4} />
         </div>
       </div>
     </div>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,10 @@
 const Counter = ({ title, number }) => {
-    return <div className="counter">
-        <div className="counter-number">{number}</div>
-        <h3 className="counter-text">{title}</h3>
-  </div>;
+  return (
+    <div className="counter">
+      <div className="counter-number">{number}</div>
+      <h3 className="counter-text">{title}</h3>
+    </div>
+  );
 };
 
 export default Counter;
