1c03f8dc246612aee426a40fcb4bcf08e440015d
Virgílio César <enjels@outlook.com>
2024-03-12 13:19:58 -0300

    adicionado 2 componentes "Title, Counter".

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
-
+import Title from "./components/Title"
 function App() {
-
-  return <div>
-    <h1>Countdown</h1>
-  </div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <Title title= "Contagem regreciva para 2023"/>
+        <div className="countdown-container">
+          <p>2 dias</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
