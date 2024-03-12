2ca12b5f360f45f0078aece448cc879bd949b91d
Virgílio César <enjels@outlook.com>
2024-03-12 17:58:49 -0300

    importação do react { createBrowserRouter, RouterProvider }

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+//pages
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
