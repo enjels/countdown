d7e11bbbd024511d16add569edbf1f55c3df0fc7
Virgílio César <enjels@outlook.com>
2024-03-13 20:09:53 -0300

    ajuste nas nomeclaturas erradas que causava problema ao trazer os dados

--- src/routes/Countdown.jsx
+++ src/routes/Countdown.jsx
@@ -1,26 +1,28 @@
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import Title from "../components/Title";
 import Counter from "../components/Counter";
-
-import { useContext } from "react";
-
 import useCountdown from "../hooks/useCountdown";
-
 import { CountdownContext } from "../context/CountdownContext";
-
-import { Navigate } from "react-router-dom";
 
 const Countdown = () => {
   const { event } = useContext(CountdownContext);
+
+  // Redireciona para a página inicial se não houver evento definido
   if (!event) return <Navigate to="/" />;
 
-  const eventTitle = event.title;
-  const eventColor = event.color;
+  const { title: eventTitle, color: eventColor, date: eventDate } = event;
 
-  const [day, hour, minute, second] = useCountdown(event.date);
+  // Obtém os valores do countdown usando o hook useCountdown
+  const [day, hour, minute, second] = useCountdown(eventDate);
+
   return (
     <>
       <Title title={eventTitle} />
-      <div className="countdown-container">
+      <div
+        className="countdown-container"
+        style={{ backgroundColor: eventColor }}
+      >
         <Counter title="Dias" number={day} />
         <Counter title="Horas" number={hour} />
         <Counter title="Minutos" number={minute} />
